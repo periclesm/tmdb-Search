d9b6d0ef2e6c9a755b0151fa865e1ef21ec23764
Perikles Maravelakis <periclesm@cloudfields.net>
2022-03-28 19:39:25 +0300

    Pagination tests

--- theMovieDBTests/APITests.swift
+++ theMovieDBTests/APITests.swift
@@ -16,11 +16,11 @@ class APITests: XCTestCase {
 	var imageEndpointURL = DataAPI().createImageEndpoint(imagePath: "/8c4a8kE7PizaGQQnditMmI1xbRp.jpg")
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
 	
 	func testSearchAPI() throws {
--- theMovieDBTests/DataTests.swift
+++ theMovieDBTests/DataTests.swift
@@ -6,16 +6,45 @@
 //
 
 import XCTest
+@testable import theMovieDB
 
 class DataTests: XCTestCase {
 
+	var timeout = 15
+	var searchTerm = "matrix"
+	
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
-
-
+	
+	func testDataPagination() throws {
+		let searchVM = SearchVM()
+		var storedDataCount = searchVM.movies.count
+		DataStore.shared.totalResults = 1 //used to kickstart the while loop below
+		
+		while searchVM.movies.count < searchVM.totalCount {
+			let operationWait = expectation(description: "Fetching API Data")
+			searchVM.getSearchData(searchTerm: searchTerm) { completed in
+				XCTAssertTrue(completed)
+				operationWait.fulfill()
+			}
+			
+			waitForExpectations(timeout: TimeInterval(self.timeout), handler: nil)
+			
+			if storedDataCount != searchVM.movies.count {
+				XCTAssertTrue(true)
+				storedDataCount = searchVM.movies.count
+				debugPrint("[tmdb-UT] Stored count: \(storedDataCount) of \(searchVM.totalCount)")
+			}
+			else {
+				XCTAssertTrue(false, "Failed in page \(searchVM.pageIndex)")
+			}
+			
+			searchVM.pageIndex+=1
+		}
+	}
 }
