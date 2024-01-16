336c34589ca56d4eb87b5b7ac368176f2fcf692d
Perikles Maravelakis <periklesm@cloudfields.net>
2024-01-16 13:10:42 +0200

    Adding a blurred backdrop to Movie Details Screen

--- theMovieDB/View/MovieVC.swift
+++ theMovieDB/View/MovieVC.swift
@@ -15,6 +15,9 @@ class MovieVC: UITableViewController {
 	
 	let vm = MovieVM()
 	
+	@IBOutlet weak var background: UIView!
+	@IBOutlet weak var backgroundImage: UIImageView!
+	
 	@IBOutlet weak var backImage: UIImageView!
 	@IBOutlet weak var movieImage: UIImageView!
 	@IBOutlet weak var movieTitle: UILabel!
@@ -25,18 +28,20 @@ class MovieVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+		self.tableView.backgroundView = background
 		setupUI()
     }
 	
 	//Assign the selected movie values into UI components.
 	func setupUI() {
-		self.movieTitle.text = vm.movie?.title
-		self.movieGenres.text = vm.genresString()
-		self.movieReleaseDate.text = vm.releaseDateString()
-		self.movieDescription.text = vm.movie?.overview
-		self.movieRatings.text = vm.ratingsString()
+		movieTitle.text = vm.movie?.title
+		movieGenres.text = vm.genresString()
+		movieReleaseDate.text = vm.releaseDateString()
+		movieDescription.text = vm.movie?.overview
+		movieRatings.text = vm.ratingsString()
 		
-		ImageManager.fetchImage(imageFile: vm.movie?.backdropPath, imageView: self.backImage, type: .backdrop)
-		ImageManager.fetchImage(imageFile: vm.movie?.posterPath, imageView: self.movieImage)
+		ImageManager.fetchImage(imageFile: vm.movie?.backdropPath, imageView: backImage, type: .backdrop)
+		ImageManager.fetchImage(imageFile: vm.movie?.posterPath, imageView: movieImage)
+		ImageManager.fetchImage(imageFile: vm.movie?.posterPath, imageView: backgroundImage)
 	}
 }
